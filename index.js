aa8ca624cedf08aae83aa8bc422b1bdbae7d62a7
rifkijones <randomcell01@gmail.com>
2022-06-09 15:05:01 +0100

    Day 1 Express Server added

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+	return res.send('Hello!');
+});
+
+app.listen(3000, () => {
+	console.log('Server running at port 3000');
+});
