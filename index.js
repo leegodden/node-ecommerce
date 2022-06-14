d7c8bf38120bb5161780a3b9406e4f3bea579fd8
rifkijones <randomcell01@gmail.com>
2022-06-14 10:13:24 +0100

    Day 3 - Added eslint

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from 'express';
-const app = express();
+import express from 'express'
+const app = express()
 
 app.get('/', (req, res) => {
-	return res.send('Hello!');
-});
+  return res.send('Hello!')
+})
 
 app.listen(3000, () => {
-	console.log('Server running at port 3000');
-});
+  console.log('Server running at port 3000')
+})
+
+// UPTO VIDEO 4
